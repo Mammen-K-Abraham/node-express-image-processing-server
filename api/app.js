93e4d6a996900c4243478f8bc4122588826677a8
Mammen Abraham <mammenezio@gmail.com>
2022-05-18 15:31:11 +0530

    router

--- api/app.js
+++ api/app.js
@@ -5,7 +5,8 @@ const app = express();
 
 let pathToIndex = path.resolve(__dirname, '../client/index.html');
 
-
+app.use('/', router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 app.use('/*',(request, response)=>{
     response.sendFile(pathToIndex);
 });
