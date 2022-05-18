64a44e3102ee14e7cdb1fbaf1cbdcd8c07eaa114
Mammen Abraham <mammenezio@gmail.com>
2022-05-18 16:07:08 +0530

    img

--- api/src/router.js
+++ api/src/router.js
@@ -3,9 +3,9 @@ const multer = require('multer');
 const path = require('path');
 const imageProcessor = require('./imageProcessor');
 
-const router = Router();
-
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
+
+const router = Router();
 
 const filename = (request, file, callback) => {
   callback(null, file.originalname);
@@ -30,18 +30,17 @@ const upload = multer({
   storage,
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
   try {
     await imageProcessor(request.file.filename);
-  } catch (error) {
-
-  }
+  } catch (error) {}
   return response.status(201).json({success: true});
 });
 
 router.get('/photo-viewer', (request, response) => {
-    response.sendFile(photoPath);
-  });
+  response.sendFile(photoPath);
+});
 
 module.exports = router;
